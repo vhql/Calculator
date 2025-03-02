b9d4eafca24a524fa09aae2639b3fd46a91f80a0
vhql <dlehddls5225@gmail.com>
2025-03-02 14:48:35 +0900

    clear

--- src/Control_IO.java
+++ src/Control_IO.java
@@ -1,12 +1,4 @@
 public class Control_IO {
-    int a,b; // 덧셈으로 2개 들어오는 숫자
-    Control_IO (int a,int b){
-        this.a = a;
-        this.b = b;
-    }
-    void add(int a, int b) {
-        int add_res = a + b;
-        System.out.println("덧셈결과 :" + add_res);
 
-    }
+
 }
